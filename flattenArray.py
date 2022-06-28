5d44dc2c201361e0e92ca199761f62341e16168d
chrosslaw <Chrosslaw@gmail.com>
2022-06-28 14:21:15 -0500

    Flatten Array solution

--- flattenArray.py
+++ flattenArray.py
@@ -3,3 +3,9 @@
 # You can assume that you will only be given one or two-dimensional arrays
 
 # For example, flatten_array([1, 2, [3, 4, 5], 6, [7, 8], 9]) should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+def flatten_array(arr):
+    return sum([[x] if type(x) != list else x for x in arr], [])
+
+
+print(flatten_array([1, 2, [3, 4, 5], 6, [7, 8], 9]))
